b47716995cd4a6e67cb8e520f39dec2d933e1de4
244267620@qq.com <wlc68911qet>
2020-05-19 09:31:25 +0800

    update data augmentation method
    Signed-off-by: 244267620@qq.com <wlc68911qet>

--- Sampling/DataAugmentation/Model/BackTranslation.py
+++ Sampling/DataAugmentation/Model/BackTranslation.py
@@ -1,26 +1,30 @@
 # coding=utf-8
 
 
-def back_translation_google (sen_list, lang='en') -> list:
+def back_translation_google (sen_list, lang='en', iter_num=1) -> list:
 	"""
 	google反向翻译
 	:param sen_list:
 	:param lang:
+	:param iter_num:
 	:return:
 	"""
 	from Sampling.DataAugmentation.Model.GoogleTranslator import GoogleTranslator
 	from tqdm import tqdm
+	fd_translator = GoogleTranslator(src='zh-CN', dest=lang)
+	bd_translator = GoogleTranslator(src=lang, dest='zh-CN')
+
+	res_list = sen_list
 
 	# forward
-	temp_list = []
-	fd_translator = GoogleTranslator(src='zh-CN', dest=lang)
-	for sen in tqdm(sen_list):
-		temp_list.append(fd_translator.translate(sen))
+	for _ in range(iter_num):
+		temp_list = []
+		for sen in tqdm(res_list):
+			temp_list.append(fd_translator.translate(sen))
 
-	# backward
-	res_list = []
-	bd_translator = GoogleTranslator(src=lang, dest='zh-CN')
-	for temp in tqdm(temp_list):
-		res_list.append(bd_translator.translate(temp))
+		# backward
+		res_list = []
+		for temp in tqdm(temp_list):
+			res_list.append(bd_translator.translate(temp))
 
 	return res_list
